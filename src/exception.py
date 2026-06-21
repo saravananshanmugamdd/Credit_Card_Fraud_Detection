ffffee6f008cd39e6f1f26bed6c081993bd06620
saravanan shanmugam <saravananshanmugamdd@gmail.com>
2026-06-21 19:25:48 +0530

    completed Preprocessing and model evaluation

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,23 @@
 import sys
 
-def error_messag_details(error, error_details=sys):
-    """this returns detailed error message with file_name and line_number"""
-    _,_,exc_tb=error_details.exc_info()
+def error_detail_message(error, error_detail:sys):
+    _,_, exc_tb=error_detail.exc_info()
 
     file_name=exc_tb.tb_frame.f_code.co_filename
 
     error_message=(
-        f"error occured in python script :[{file_name}]"
-        f"erorr occured in line number :[{exc_tb.tb_lineno}]"
-        f"error message :[{str(error)}]"
+        f"error occured in file :{file_name}"
+        f"error in line number :{exc_tb.tb_lineno}"
+        f"error message :{error}"
     )
-
     return error_message
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message=error_messag_details(
+        self.error_message=error_detail_message(
             error_message,
-            error_detail
-        )
-
+            error_detail)
+        
     def __str__(self):
         return self.error_message
